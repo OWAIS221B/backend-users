15b0dc04afc815bb2ff80a66898f066f2b8657a6
Owais <inform.os221@gmail.com>
2025-05-28 23:43:13 +0530

    export module snippet

--- api/users.js
+++ api/users.js
@@ -59,7 +59,10 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
+module.exports.handler = serverless(app);
